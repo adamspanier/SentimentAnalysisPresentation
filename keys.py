e86dde61e4c8691494919be4bfa347ab8cf6a89e
Adam Spanier <aspanier@unomaha.edu>
2022-06-20 14:09:45 -0500

    Update and rename keysEmpty.py to keys.py

--- keys.py
+++ keys.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+#Please keep the quotation marks when pasting key values
+
 key = "Get From Twitter Account"
 keySecret = "Get From Twitter Account"
 token = "Get From Twitter Account"
